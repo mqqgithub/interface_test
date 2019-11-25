eb7f4349ddb57482127de3a3af5024b1e9b66dad
mqqgithub <maqq1234>
2019-11-25 22:20:07 +0800

    123123

--- common/configHttp.py
+++ common/configHttp.py
@@ -6,22 +6,28 @@ import json
 from common.log import TestLog
 
 logger = TestLog().get_log()
-class RunMain():
 
+
+class RunMain():
     # 定义一个方法，传入需要的参数url和data
     def send_post(self, url, data):
+        '''
         # 参数必须按照url、data顺序传入,data是字典类型
-        result = requests.post(url=url, data=data).json()# 因为这里要封装post方法，所以这里的url和data值不能写死
+        # 因为这里要封装post方法，所以这里的url和data值不能写死
+        '''
+        result = requests.post(url=url, data=data).json()
         res = json.dumps(result, ensure_ascii=False, sort_keys=True, indent=2)
         return res
 
     def send_get(self, url, data):
         result = requests.get(url=url, data=data)
-        # ensure_ascii:，当它为True的时候，所有非ASCII码字符显示为\uXXXX序列，
+        '''
+        # ensure_ascii:，当它为True的时候，所有非ASCII码字符显示为序列，
         # 只需在dump时将ensure_ascii设置为False即可，此时存入json的中文即可正常显示
         # sort_keys：将数据根据keys的值进行排序。
         # indent：应该是一个非负的整型，如果是0就是顶格分行显示，如果为空就是一行最紧凑显示，
         # 否则会换行且按照indent的数值显示前面的空白分行显示，这样打印出来的json数据也叫pretty-printed json
+        '''
         res = json.dumps(result, ensure_ascii=False, sort_keys=True, indent=2)
         return res
 
@@ -31,10 +37,10 @@ class RunMain():
         result = None
         if method == 'post':
             result = self.send_post(url, data)
-            logger.info(str(result))
+            # logger.info(str(result))
         elif method == 'get':
             result = self.send_get(url, data)
-            logger.info(str(result))
+            # logger.info(str(result))
         else:
             print("method值错误！！！")
             logger.info("method值错误！！！")
@@ -44,4 +50,4 @@ class RunMain():
 if __name__ == '__main__':
     # 通过写死参数，来验证我们写的请求是否正确
     result = RunMain().run_main('post', 'http://127.0.0.1:8888/login', {'name': 'xiaoming', 'pwd': '111'})
-    print(result)
+    print('result:', result)
--- test_api.py
+++ test_api.py
@@ -1,14 +1,17 @@
 import flask
 import json
 from flask import request
+import pytest
 
 '''
 flask： web框架，通过flask提供的装饰器@server.route()将普通函数转换为服务
 '''
 # 创建一个实例，服务，把当前这个python文件当做一个服务
 server = flask.Flask(__name__)
+
+
 # @server.route()可以将普通函数转变为服务 登录接口的路径、请求方式
-@server.route('/login', methods=['get', 'post'])  #@装饰器
+@server.route('/login', methods=['get', 'post'])
 def login():
     # 获取通过url请求传参的数据  http://127.0.0.1:8888/login?name=xiaoming&pwd=11199
     username = request.values.get('name')
@@ -18,7 +21,9 @@ def login():
     if username and pwd:
         if username == 'xiaoming' and pwd == '111':
             resu = {'code': 200, 'message': '登录成功'}
-            return json.dumps(resu, ensure_ascii=False)  # json是把字符串格式转为字典，dumps是将字典转换字符串   #json.dumps序列化时对中文默认使用的ascii编码.想输出真正的中文需要指定ensure_ascii = False：
+            # json是把字符串格式转为字典，dumps是将字典转换字符串
+            # json.dumps序列化时对中文默认使用的ascii编码.想输出真正的中文需要指定ensure_ascii = False：
+            return json.dumps(resu, ensure_ascii=False)
         else:
             resu = {'code': -1, 'message': '账号密码错误'}
             return json.dumps(resu, ensure_ascii=False)
